d8ea934e9db6cd7c4fcf6bae638da41871224f80
Miguel Angel Maffet Wall <miguelangelmaffet@gmail.com>
2017-06-29 05:39:00 -0400

    Programa base

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+#
+#	==================
+#	Algoritmo genetico
+#	==================
+#
+#	Miguel Angel Maffet Wall
+#	Junio 2017
+#
+
+def prueba( str ):
+   "This prints a passed string into this function"
+   print str
+   return
+
+def main():
+   prueba("Hola todos")
+   return 0;
+
+main()
